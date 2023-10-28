4b9da5be9e27aa5b7402a47bff4b508c195d6743
Florin Asavei <florin.asavei@exghts.com>
2023-10-28 20:32:15 +0300

    relative json path

--- apps/invoiceViewer/src/services/receivables.ts
+++ apps/invoiceViewer/src/services/receivables.ts
@@ -3,7 +3,7 @@ import httpService from './http'
 
 const getAll = async():Promise<IReceivableDTO[]>=>{
     //TODO: this should come from an API but it is currently restricted by CORS
-    const response = await httpService.get('/sample-receivables-data.json')
+    const response = await httpService.get('./sample-receivables-data.json')
     return response.data
 }
 
